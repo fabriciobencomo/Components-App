c548c94d9a8c6014e9f23684d73e38574640afec
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-01-05 00:23:28 -0400

    themebutton created

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,18 @@
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
 import { View, Text } from 'react-native';
 
 const Animation101Screen = () => {
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView>
+      <ThemedButton className='mb-5' onPress={() => console.log('fadein')}>
+        Fade In
+      </ThemedButton>
+
+      <ThemedButton className='mb-5' onPress={() => console.log('fadeout')}>
+        Fade Out
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
---
+++ presentation/shared/ThemedButton.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Pressable, PressableProps } from 'react-native'
+import React from 'react'
+
+interface Props extends PressableProps {
+  className?: string;
+  children: string;
+}
+const ThemedButton = ({children, className, ...rest}: Props) => {
+  return (
+    <Pressable className={`bg-light-primary dark:bg-dark-primary items-center rounded-xl px-6 py-2 active:opacity-90 ${className}`}>
+      <Text className='text-white text-2xl'>{children}</Text>
+    </Pressable>
+  )
+}
+
+export default ThemedButton
